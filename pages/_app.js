788c97a3c292d729c304bd0dd419c6abea12229d
fhammerschlag <feedeh20@gmail.com>
2025-04-04 15:33:25 -0300

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+
+import '../styles/globals.css';
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThirdwebProvider activeChain="polygon">
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
