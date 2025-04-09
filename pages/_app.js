d15deb2383fd44a2f51f14b82fc0b86fa2d6b2a3
fhammerschlag <feedeh20@gmail.com>
2025-04-09 11:51:48 -0300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,16 @@
-import "/styles/globals.css";
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+import { useEffect } from "react";
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://www.chatbase.co/embed.min.js";
+    script.id = "mkCA4neBR-ERcWq2nzTYD"; // ID de tu bot
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
